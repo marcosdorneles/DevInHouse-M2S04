3373e8aa58bab2e9e9f4affd9050adf23ce417f4
Marcos Guilherme Dorneles Staub <marcosguilhermedornelesstaub@MacBook-Air-de-Marcos.local>
2023-05-02 14:14:51 -0300

    [M2S04] Ex 1 - Modificadores de acesso: Saudação Pessoa

---
+++ M2S04 - exercicios semanais/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace M2S04___exercicios_semanais
+{
+	public class Pessoa
+	{
+		public Pessoa(String Nome, int Idade, String Endereco)
+		{
+			this.Nome = Nome;
+			this.Idade = Idade;
+			this.Endereco = Endereco;
+		}
+
+        public Pessoa()
+        {
+        }
+
+        private string Nome { get; set; }
+		protected int Idade { get; set; }
+        public string Endereco { get; set; }
+
+        public void saudacao()
+		{
+			Console.WriteLine($"Olá, meu nome é {Nome} e eu tenho {Idade} anos");
+		}
+	}
+}
+
--- M2S04 - exercicios semanais/Program.cs
+++ M2S04 - exercicios semanais/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using M2S04___exercicios_semanais;
 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Pessoa pessoa1 = new Pessoa("Marcos", 30, "Rua A, 123");
+
+        pessoa1.saudacao();
+    }
+}
